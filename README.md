e4d779c2a42d60721746c9e5999ad0f7f55c4fff
JRfushaoshuai <fushaoshuai@joyrry.com>
2020-05-21 14:03:33 +0800

    设置请求静态文件

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # node-practice
 学习nodejs
-
+# 下载依赖
+npm install
 # 运行：
-node server.js
+node start
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Express'
+        title: '砥砺前行'
     });
 });
 
